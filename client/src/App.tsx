de7ce677945b00f70964a63f455b8e5677d0ff3d
X-Abby-X <abbyabbylui@gmail.com>
2025-08-06 12:39:06 -0400

    added scroll to top for top view on load

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Event from './pages/Events'
+import ScrollToTop from './components/ScrollTop'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/event" element={<Event />} />
-      </Routes>
-    </Router>
+    <>
+
+      <Router>
+        <ScrollToTop/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/event" element={<Event />} />
+        </Routes>
+      </Router>
+    </>
+    
   )
 }
 
