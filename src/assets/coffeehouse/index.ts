eb34764541a7bf25e26526f4153b0cc6ce3a4db2
X-Abby-X <abbyabbylui@gmail.com>
2025-09-11 00:40:02 -0400

    changed coffee image to webp, rebuild

--- src/assets/coffeehouse/index.ts
+++ src/assets/coffeehouse/index.ts
@@ -1,12 +1,12 @@
-import first from './1.jpg'
-import second from './2.jpg'
-import third from './3.jpg'
-import fourth from './4.jpg'
-import fifth from './5.jpg'
-import team from './team.jpg'
-import mentor from './mentor.jpg'
-import promo1 from './mentorship_coffeehouse_promo1.png'
-import promo2 from './mentorship_coffeehouse_promo2.png'
+import first from './1.webp'
+import second from './2.webp'
+import third from './3.webp'
+import fourth from './4.webp'
+import fifth from './5.webp'
+import team from './team.webp'
+import mentor from './mentor.webp'
+import promo1 from './mentorship_coffeehouse_promo1.webp'
+import promo2 from './mentorship_coffeehouse_promo2.webp'
 
 
 export {first, second, third, fourth, fifth, team, mentor, promo1, promo2};
