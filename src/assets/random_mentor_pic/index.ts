22de114999c9e735d1488787dd38d79c74b15f98
X-Abby-X <abbyabbylui@gmail.com>
2025-09-11 01:04:15 -0400

    rebuild with mentor webp image

--- src/assets/random_mentor_pic/index.ts
+++ src/assets/random_mentor_pic/index.ts
@@ -1,12 +1,12 @@
-import first from './1.jpg';
-import second from './2.jpg';
-import third from './3.jpg';
-import forth from './4.jpg';
-import fifth from './5.jpg';
-import sixth from './6.jpg';
-import seventh from './7.jpg';
-import eighth from './8.jpg';
-import ninth from './9.jpg';
-import tenth from './10.jpg';
+import first from './1.webp';
+import second from './2.webp';
+import third from './3.webp';
+import forth from './4.webp';
+import fifth from './5.webp';
+import sixth from './6.webp';
+import seventh from './7.webp';
+import eighth from './8.webp';
+import ninth from './9.webp';
+import tenth from './10.webp';
 
 export {first, second, third, forth, fifth, sixth, seventh, eighth, ninth, tenth};
