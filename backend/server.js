9d55bb7b1d77a0fb16a1c0b6ae0a70d477c74510
bunmann <jungd611@gmail.com>
2026-08-07 20:46:35 -0400

    Revert alter sync

--- backend/server.js
+++ backend/server.js
@@ -1018,6 +1018,6 @@ app.get("/leaderboard/export", verifyAdmin, async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-sequelize.sync({ alter: true }).then(() => {
+sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
