9c8817166150c3a312b84924a7029fa88703630e
bunmann <jungd611@gmail.com>
2026-09-19 14:23:49 -0400

    Removed uthsdc.php file

--- frontend/dist/uthsdc.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirect directly to UTHSDC Zeffy Ticketing Page
-header("Location: https://www.zeffy.com/en-CA/ticketing/university-of-toronto-high-school-design-competition-2t6", true, 302);
-exit();
-?>
